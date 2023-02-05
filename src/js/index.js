103983aeeab8742b206eb7b0c51e65c1921a595e
romanconstantin1 <romanconstantin1@gmail.com>
2023-02-05 13:07:29 +0000

    added state switching code

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,29 @@
 //import react into the bundle
-import React from "react";
+import React, { useState, useEffect }from "react";
 import ReactDOM from "react-dom";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+const TrafficLight = () => {
+    const useStyle = {visibility: "hidden"}
+    
+    const [isSelected, yPos] = useState(useStyle);
+
+    const colorSelector = (newY) => {yPos(newY)}
+
+    return (
+        <div>
+            <div className="trafficLight">
+                <div className="lightBkgrnd">
+                    <div className="glowCirc" style={isSelected}></div>
+                    <button className="redLight" onClick={() => (colorSelector({visibility: "visible", marginTop: 13}))}></button>
+                    <button className="yellowLight" onClick={()=> (colorSelector({visibility: "visible", marginTop: 142}))}></button>
+                    <button className="greenLight" onClick={() => (colorSelector({visibility: "visible", marginTop: 271}))}></button>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
